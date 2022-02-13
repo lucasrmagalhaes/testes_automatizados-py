50efd007ec72d83c70f7d9faf6901fce5e505c52
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-13 17:25:32 -0300

    Começando com testes de unidade

---
+++ README.md
@@ -0,0 +1,6 @@
+### TDD
+
+Rodar o comando em cima da Classe para iniciar um teste
+```
+Ctrl + Shift + t
+```
---
+++ src/leilao/test_dominio.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+
+
+class TestAvaliador(TestCase):
+    def test_avalia(self):
+        lucas = Usuario('Lucas')
+        fulano = Usuario('Fulano')
+
+        lance_do_lucas = Lance(lucas, 100.0)
+        lance_do_fulano = Lance(fulano, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_lucas)
+        leilao.lances.append(lance_do_fulano)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
