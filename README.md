1097df64c41a511a3971c254dcb7ecc635416472
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-13 17:47:41 -0300

    Mais testes e classes de equivalência

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-**Testes automatizados:** TDD com Python.
+### Testes automatizados:** TDD com Python
 
-Rodar o comando em cima da Classe para iniciar um teste
-```
-Ctrl + Shift + t
-```
-
-Rodar os testes pelo terminal:
-```python
-python -m unittest src/leilao/test_dominio.py
-```
+<details>
+    <summary><strong>Comandos</strong></summary>
+    <br>
+    Rodar o comando em cima da Classe para iniciar um teste
+    ```
+    Ctrl + Shift + t
+    ```
+    Rodar os testes pelo terminal:
+    ```python
+    python -m unittest src/leilao/test_dominio.py
+    ```
+</details>
--- src/leilao/test_dominio.py
+++ src/leilao/test_dominio.py
@@ -46,3 +46,41 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
+        lucas = Usuario('Lucas')
+
+        lance = Lance(lucas, 150)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150.0, avaliador.menor_lance)
+        self.assertEqual(150.0, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+        lucas = Usuario('Lucas')
+        fulano = Usuario('Fulano')
+        beltrano = Usuario('Beltrano')
+
+        lance_do_lucas = Lance(lucas, 100.0)
+        lance_do_fulano = Lance(fulano, 150.0)
+        lance_do_beltrano = Lance(beltrano, 200.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_lucas)
+        leilao.lances.append(lance_do_fulano)
+        leilao.lances.append(lance_do_beltrano)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
